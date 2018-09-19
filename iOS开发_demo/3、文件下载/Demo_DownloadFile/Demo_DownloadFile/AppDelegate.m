083612b2607da0d8270368e4c1f4f92dce38ad95
lee <lee@leedeMacBook-Pro.local>
2018-09-19 18:16:40 +0800

    1、多文件下载

---
+++ iOS开发_demo/3、文件下载/Demo_DownloadFile/Demo_DownloadFile/AppDelegate.m
@@ -0,0 +1,25 @@
+/**
+ 功能、文件下载
+ 
+ 
+ 参考文章
+    https://blog.csdn.net/hero_wqb/article/details/80407478
+    https://github.com/HeroWqb/HWDownloadDemo
+ */
+
+#import "AppDelegate.h"
+
+@interface AppDelegate ()
+
+@end
+
+@implementation AppDelegate
+
+
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+    return YES;
+}
+
+
+
+@end
